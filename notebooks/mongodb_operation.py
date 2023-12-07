962390a58e09fd3c676ba0eadd4c4130bf1de76a
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-12-07 14:41:20 +0530

    mongodb operation added

---
+++ notebooks/mongodb_operation.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import pandas as pd
+from json import loads
+import os
+
+
+data_frame = pd.read_csv(r"G:\100-days-of-dl\Krish_Naik\FSDS_Ineuron_Course\projects\Insurance-Premium-Price-Prediction\notebooks\data\insurance.csv")
+
+records = list(loads(data_frame.T.to_json()).values())
+
+client = MongoClient("mongodb+srv://harshal:harshal123@cluster0.gimlrjo.mongodb.net/?retryWrites=true&w=majority")
+
+database_name = client["Harshal"]
+
+collection = database_name["insurance"]
+
+collection.insert_many(records)
+
+for i in collection.find():
+    print(i)
