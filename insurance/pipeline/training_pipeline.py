af6310ade182e61a390d6b29e4cc9e5bd581f1b4
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-12-08 15:20:53 +0530

    data validation added

--- insurance/pipeline/training_pipeline.py
+++ insurance/pipeline/training_pipeline.py
@@ -2,8 +2,9 @@ import os, sys
 from typing import Tuple
 
 from insurance.components.data_ingestion import DataIngestion
-from insurance.entity.config_entity import DataIngestionConfig
-from insurance.entity.artifact_entity import DataIngestionArtifact
+from insurance.components.data_validation import DataValidation
+from insurance.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from insurance.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 from insurance.exception import CustomException
 from insurance.logger import logging
 from pandas import DataFrame
@@ -12,6 +13,7 @@ from pandas import DataFrame
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -35,6 +37,28 @@ class TrainingPipeline:
         except Exception as e:
             raise CustomException(e, sys) from e
 
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        try:
+            logging.info(
+                "Entered the start_data_validation method of TrainPipeline class"
+            )
+            logging.info("Getting the data from mongodb")
+            data_validation = DataValidation(
+                data_ingestion_artifact = data_ingestion_artifact,
+                data_validation_config = self.data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Got the train_set and test_set from mongodb")
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+            return data_ingestion_artifact
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
 
     def run_pipeline(self,) -> None:
         """
@@ -42,5 +66,6 @@ class TrainingPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise CustomException(e, sys) from e
